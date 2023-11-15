d2b5b4046aab66c5c88778078af7b7351591e6b7
vitor ozorio <vitormanoelbasilio357@gmail.com>
2023-11-15 14:14:18 -0300

    teste

--- html/aula da facul/aula 3/function.js
+++
@@ -1,6 +0,0 @@
-
- 
-    let hora = new Date();
-    document.body.innerHTML = "<h1> a hora agora é : " + hora.getHours() + ":" + hora.getMinutes() + 
-        ":" + hora.getSeconds() + "<h1>"
-    
